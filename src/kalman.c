740f98503e623ef4b9985aec8db40befb5448c1f
Pritindra <pritindra.d@gmail.com>
2021-07-25 14:59:00 +0530

    initialized folders and files

---
+++ src/kalman.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+// kalman filter function ,i.e, all equations 
+// func name (float or int objects)
+// resultng equations and results.
+
+double kf(double* P, double* Q_est) {
+    //for loop
+        // equations
+    //return results
+}
---
+++ src/kf.py
@@ -0,0 +1,18 @@
+import numpy as np
+import glob
+import ctypes
+
+libfile = glob.glob('build/*/*kf.so')[0]
+
+mylib = ctypes.CDLL(libfile)
+
+# including function from kalman.c
+mylib.kf.restype = numpy.ctypeslib.ndpointer(dtype=numpy.float64)
+mylib.kf.argtypes = [numpy.ctypeslib.ndpointer(dtype=numpy.float64),numpy.ctypeslib.ndpointer(dtype=numpy.float64)]
+
+# calling function
+# make class of kalman filter
+# supporting of numpy arrays input to the c function
+# make usefull for any type of array
+
+
