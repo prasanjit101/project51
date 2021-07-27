0e99354cd95362e8b7d9d141fdeb8dd4e80e44c8
Prasanjit dutta <57594105+prasanjit101@users.noreply.github.com>
2021-07-27 20:22:29 +0530

    Update kalman.hpp

---
+++ kalman/kalman.hpp
@@ -0,0 +1,28 @@
+#if !defined(KALMAN_HPP)
+#define KALMAN_HPP
+
+
+#include <iostream>
+#include "kalman.cpp"
+
+
+class kalman
+{
+private:
+    /* data */
+
+
+public:
+    kalman(/* args */);
+    ~kalman();
+};
+
+kalman::kalman(/* args */)
+{
+}
+
+kalman::~kalman()
+{
+}
+
+#endif // KALMAN_HPP
